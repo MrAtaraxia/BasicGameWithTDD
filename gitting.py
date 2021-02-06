05617cb3cdc05b34f92dd7aa3044e31f7db73db5
wchnowski <william.chnowski@gmail.com>
2021-02-06 08:46:34 -0500

    auto comment at 2021-02-06_08-46-34

--- gitting.py
+++ gitting.py
@@ -4,16 +4,29 @@ import os
 from datetime import datetime, date
 import datetime
 import time
-now = datetime.datetime.now()
-my_time = f"{now.year:04}-{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}-{now.second:02}"
 
-thing1 = " auto comment"
-my_commands = f"""
-git add --all
-git commit -m "{thing1} at {my_time}"
 
-"""
+def main():
+    now = datetime.datetime.now()
+    #now2 = datetime.datetime(year=2021, month=2, day=2, hour=24, minute=10, second=10)
+    #print(now2)
+    my_time = f"{now.year:04}-{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}-{now.second:02}"
 
-my_location = f"pwd"
+    thing1 = " auto comment"
+    my_commands = f"""
+    git add --all
+    git commit -m "{thing1} at {my_time}"
+    """
+    os.system(my_commands)
+    last_sent = now
+    while True:
+        now = datetime.datetime.now()
+        if now.hour > last_sent.hour or (last_sent.hour == 23 and now.hour == 0):
+            os.system(my_commands)
+            last_sent = now
+        time.sleep(5)
+        print(my_time)
 
-os.system(my_commands)
+
+if __name__ == "__main__":
+    main()
